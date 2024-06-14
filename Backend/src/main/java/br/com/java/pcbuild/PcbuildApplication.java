7ea6eadab596e96ab80137cce88e1121f2196649
AngeloGCapra <angelo.gabriel.capra@gmail.com>
2024-06-13 21:21:28 -0300

    Criação do projeto back-end no Spring Initializr

---
+++ Backend/src/main/java/br/com/java/pcbuild/PcbuildApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.pcbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcbuildApplication.class, args);
+	}
+
+}
